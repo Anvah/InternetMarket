20e48717d3bf01fc36eae835455c7026c3b76f8c
Anvah <77714260+Anvah@users.noreply.github.com>
2022-09-30 18:13:20 +0300

    Update AccountController.cs

--- Market/Controllers/AccountController.cs
+++ Market/Controllers/AccountController.cs
@@ -111,14 +111,11 @@ namespace Market.Controllers
         }
         private async Task Authenticate(string userName)
         {
-            // создаем один claim
             var claims = new List<Claim>
             {
                 new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
             };
-            // создаем объект ClaimsIdentity
             ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
-            // установка аутентификационных куки
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
         }
 
